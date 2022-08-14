7cd28298cd10b052bb4bf164ab5aba168c755024
Mrunal <80758264+msy1717@users.noreply.github.com>
2022-08-14 14:33:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,51 @@ async def start_(event):
     )
 
 
+from telethon.tl.functions.channels import EditBannedRequest
 
+from telethon.tl.types import ChatBannedRights
+
+@bot.on(events.NewMessage(incoming=True, pattern="^/kickall"))
+
+async def ban_(event):
+
+  nikal = await event.get_chat()
+
+  chutiya = await event.client.get_me()
+
+  admin = nikal.admin_rights
+
+  creator = nikal.creator
+
+  if not admin and not creator:
+
+    await event.edit(" Don't have sufficient permission 🧐 u noob 😑😑")
+
+    return
+
+  await event.edit("Doing Nothing 🙃🙂")  
+
+  everyone = await event.client.get_participants(event.chat_id)
+
+  for user in everyone:
+
+    if user.id == chutiya.id:
+
+      pass
+
+    try:
+
+      await event.client(EditBannedRequest(event.chat_id,int(user.id),ChatBannedRights(until_date=None,view_messages=True),))
+
+    except Exception as e:
+
+      await event.edit(str(e))
+
+    await sleep(0.5)
+
+  await event.edit("Nothing Happend here🙃🙂")
+
+  
 
 logging.info("\n\nBot has started.\n(c) @Godmrunal")
 
